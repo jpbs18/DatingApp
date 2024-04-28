659d7f84cc0ab8baf402778b96e7ddb666f59805
João Santos <joao.santos@mindswap.academy>
2024-04-28 15:18:27 +0100

    Added UI skeleton

--- API/Program.cs
+++ API/Program.cs
@@ -5,9 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddCors();
 
 var app = builder.Build();
 
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
